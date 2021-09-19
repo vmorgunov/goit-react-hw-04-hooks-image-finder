a5f0e1feea97c78a6e2cc4ccf7ba91254b942708
Vitalii Morgunov <utlab13@gmail.com>
2021-09-19 11:48:58 +0200

    Fix bug scroll on 1 page

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,14 @@ export default function App() {
 
         setImages(prevState => [...prevState, ...data.hits]);
       })
+      .then(() => {
+        if (page > 1) {
+          handleScroll();
+        }
+      })
       .catch(error => console.log(error.message))
       .finally(() => {
         setIsLoading(false);
-        handleScroll();
       });
   }, [query, page]);
 
